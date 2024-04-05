80afd71b43a8f62e8eaf7f1ea4c67c8fa963554a
NikitaVolia <volanskijnikita2@gmail.com>
2024-04-05 17:24:37 +0200

    add fixs to getNearest and callElivator methods of Elevator class

--- Elevator.cpp
+++ Elevator.cpp
@@ -16,15 +16,21 @@ Elevator& Elevator::getUpper(Elevator& other)
 
 Elevator& Elevator::getNearest(Elevator& other, int pStage)
 {
-	return (stage - pStage) * (stage - pStage) < (other.stage - pStage) * (other.stage - pStage) ? *this : other;
+	return std::abs(stage - pStage) < std::abs(other.stage - pStage) ? *this : other;
 }
 
 void Elevator::callElivator(int pStage)
 {
 	if (pStage < minStage || pStage > maxStage)
+	{
+		std::cout << "[Elevator info :" << this << " ] Invalid parameter pStage\n";
 		return;
+	}
 	if (!active)
+	{
+		std::cout << "[Elevator info :" << this << " ] Elevator is currently off\n";
 		return;
+	}
 
 	stage = pStage;
 }
